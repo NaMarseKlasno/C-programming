c69099e1d82a75b7556732e31cf58b04549755c5
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-07 10:59:12 +0000

    Update main.c

--- ps1/main.c
+++ ps1/main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-#include "playfair.h"
 #include <stdlib.h>
+#include "playfair.h"
+#include "bmp.h"
 
 int main(void)
 {
     char *encrypted, *decrypted;
 
-    // even length of string
-    encrypted = playfair_encrypt("please", "Taxxxiii");
+    encrypted = playfair_encrypt("world", "Hello");
     printf("%s\n", encrypted);
-    // "Hello world" --> "HELXLOVORLDX"
-    // IS JZ JQ XN TK JC
-    
-    decrypted = playfair_decrypt("Password", encrypted);
-    printf("%s", decrypted);
-    // TAXIPLEASE
+    // "Hello" --> "HELXLO"
+    // JB RY DR
+    decrypted = playfair_decrypt("world", encrypted);
+    printf("%s\n", decrypted);
+    // HELXLO
     free(encrypted);
     free(decrypted);
+    
+    
+    char* reversed = reverse("Hello world!");
+    printf("%s\n", reversed);
+    // "!DLROW OLLEH"
+    //free(reversed);
     return 0;
 }
