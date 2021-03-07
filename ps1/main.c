9785f0e4d1e88dfc03272f0a7311f6965c37be79
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-07 22:37:27 +0000

    Update main.c

--- ps1/main.c
+++ ps1/main.c
@@ -5,22 +5,33 @@
 
 int main(void)
 {
+    // "A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z"
+    // "0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25"
     char *encrypted, *decrypted;
 
-    encrypted = playfair_encrypt("world", "Hello");
-    printf("%s\n", encrypted);
-    // "Hello" --> "HELXLO"
-    // JB RY DR
-    decrypted = playfair_decrypt("world", encrypted);
-    printf("%s\n", decrypted);
-    // HELXLO
+    encrypted = playfair_encrypt("please", "Taxxxiii");
+    printf("%s", encrypted);
+    // "Taxxxiii" --> "TAXXXIIXIX"
+    // RS EE VJ JV JV
+    decrypted = playfair_decrypt("please", encrypted);
+    printf("%s", decrypted);
+    // TAXXXIIXIX
     free(encrypted);
     free(decrypted);
-    
     
     char* reversed = reverse("Hello world!");
     printf("%s\n", reversed);
     // "!DLROW OLLEH"
-    //free(reversed);
+
+    
+    char* encrypted2;
+
+    // basic test with long text
+    encrypted2 = vigenere_encrypt("CoMPuTeR", "Hello world!");
+    printf("%s\n", encrypted2);
+    // "JSXAI PSINR!"
+
+    
+
     return 0;
 }
