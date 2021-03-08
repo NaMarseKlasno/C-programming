29b9864fd805dd86e727300d2586552877b8d0e8
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-08 13:55:33 +0000

    Update bmp.c

--- ps1/bmp.c
+++ ps1/bmp.c
@@ -16,7 +16,8 @@
 
 
 char* reverse(const char* text){
-    if (strlen(text) == 0 || text == NULL) return NULL;
+    if (text[0] == '\0'  || text == NULL) return NULL;
+    
     int countAlpha = 0;
     for (int i = 0; i < strlen(text); ++i) if (isalpha(text[i])) countAlpha++;
     if (countAlpha == 0) return NULL;
@@ -38,4 +39,86 @@ char* reverse(const char* text){
     return reverseText;
 }
 
+
+char* vigenere_encrypt(const char* key, const char* text){
+    if (key[0] == '\0' || key == NULL) return NULL;
+    if (text[0] == '\0'  || text == NULL) return NULL;
+    
+    char mainKey[strlen(text)];
+    for (int i = 0, j = 0, c = 0; c < strlen(text); ++i) {
+        if (isalpha(text[i])){
+            mainKey[c] = key[j];
+            ++j;
+            //printf("%c\n", mainKey[c]);
+            if (j == strlen(key)) j = 0;
+            ++c;
+        }else {
+            mainKey[c] = ' ';
+            c++;
+        }
+    }
+    //printf("%s\n", mainKey);
+    //printf("%s\n", text);
+
+
+    int numKey[strlen(mainKey)];
+    for (int i = 0; i < strlen(mainKey); ++i) numKey[i] = 0;
+    
+    for (int i = 0; i < strlen(mainKey); ++i){
+        for (int j = 0; j < 26; ++j) {
+            if (ALPHAB[j] == toupper(mainKey[i])) {
+                numKey[i] = j;
+            }
+            else if (!isalpha(mainKey[i])){
+                numKey[i] = -1;
+                //printf("[%d] ", numKey[i]);
+            }
+        }
+    }
+    //printf("\n");
+    //for (int i = 0; i < strlen(text); ++i) printf("%i ", numKey[i]);
+    
+    //printf("\n");
+   
+    int numText[strlen(text)];
+    for (int i = 0; i < strlen(text); ++i){
+        for (int j = 0; j < 26; ++j) {
+            if (ALPHAB[j] == toupper(text[i])) {
+                numText[i] = j;
+            }
+            else if (!isalpha(text[i]))
+                numText[i] = -1;
+        }
+    }
+    //for (int i = 0; i < strlen(text); ++i) printf("%i ", numText[i]);
+    
+    //printf("\n");
+
+    
+    int resInt[strlen(text)];
+    for (int i = 0; i < strlen(text); ++i) resInt[i] = 0;
+    
+    for (int i = 0; i < strlen(text); ++i) {
+        resInt[i] = numText[i] + numKey[i];
+        if (resInt[i] >= 26) resInt[i] -= 26;
+        if (numText[i] == -1) resInt[i] = -1;
+        if (numKey[i] == -1) resInt[i] = -1;
+    }
+    //for (int i = 0; i < strlen(text); ++i) printf("%i ", resInt[i]);
+    //printf("\n");
+
+    char* resSting = checkNull(calloc(strlen(text), sizeof(char)));
+
+    for (int i = 0; i < strlen(text); ++i) {
+        for (int j = 0; j < 26; ++j) {
+            if ( j == resInt[i] ) resSting[i] = ALPHAB[j];
+            if (resInt[i] == -1) resSting[i] = text[i];
+        }
+    }
+    
+    //printf("%s - resString\n", resSting);
+    
+    return resSting;
+}
+// "JSXAI PSINR!"
 
