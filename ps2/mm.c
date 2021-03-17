19e651aeead14e85592e6cf4e424d65f7f132444
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-17 21:30:45 +0000

    Upload New File

---
+++ ps2/mm.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void mm(void);
+
+
+int main(void)
+{
+    // Mighty Method
+    mm();
+    
+    // Perfect Isolation
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+void mm(void)
+{
+    time_t start, end;
+    
+    long long n = 0;
+    long long k, i, m, *arr = NULL;
+    scanf("%lld", &n);
+    time (&start);
+
+    for (m = 0, k = n; k != 0; m++) k /= 10;
+    //printf("1st m = %lld\n", m);
+   
+    arr = (long long*)calloc(10*m+1, sizeof(int));
+    if (arr == NULL) {
+        free(arr);
+        arr = NULL;
+        exit(1);
+    }
+    
+    for (i = 0, k = n; i < m; i++) {
+        arr[i] = k % 10;
+        k = k / 10;
+    } //for ( i = m - 1; i >= 0; i--) printf("%lld ",arr[i]); printf("\n");
+    
+    
+    //printf("m = %lld\n", m);
+
+    for (int j = 0; m > 1; ++j) {
+        n = 0;
+        for ( i = m - 1; i >= 0; i--) n += arr[i];
+        
+        for (m = 0, k = n; k != 0; m++) k /= 10;
+
+        //printf("in end n = %lld\n", n);
+        //printf("in end m = %lld\n", m);
+        
+        arr = (long long*)realloc(arr, (10* m * sizeof(int))+1);
+        if (arr == NULL) {
+            free(arr);
+            arr = NULL;
+            exit(1);
+        }
+        for (i = 0; i < m; i++) {
+            arr[i] = n % 10;
+            n = n / 10;
+        }
+    
+    }
+    n = arr[0];
+    printf("%lld\n", n);
+    free(arr);
+    arr = NULL;
+
+    time (&end);
+    double dif = difftime(end, start);
+    printf("Time = %lf\n", dif);
+}
