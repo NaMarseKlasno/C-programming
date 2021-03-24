eacdb1d43b3e5a91a1dd6b88efc08832103c19d9
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-24 14:57:11 +0000

    Update racer.c

--- ps2/racer.c
+++ ps2/racer.c
@@ -1,148 +1,66 @@
-// Stanislav Voloshyn
-// @pchoul - telegram
-
-
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
-
-int bubbleSort (int arr[][2], int n);
-
-bool testValue(long double k, int arr[][2], int t, int parts, double *value, short count) {
-    long double res = 0;
-    for (int i = 0; i < parts; ++i) {
-        if ( ((double)arr[i][1] + k) != 0 ) {
-            res += (double)arr[i][0] / ((double)arr[i][1] + k);
-        }
-    }
-    //printf("k: %f -> time: %f\n", k, res);
-
-    if (count == 1) {
-        if (*value > res) *value = -1;
-        else *value = res;
-    }
-
-    if (count == 2) {
-        static short nw = 0;
-        if (*value <= (double)t && nw != 0) {
-            *value = -1;
-        }
-        else *value = res;
-        nw = 1;
-
-    }
-
-    if (res == t) return true;
-    return false;
-}
-
-
-int rac (void)
+struct lip
 {
-    int nParts = 0, q = 0, t = 0;
-    short count = 1;
-    scanf("%d %i", &nParts, &t);
-
-    int mainArr[nParts][2];
-    for (int i = 0; i < nParts; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            scanf("%i", &q);
-            mainArr[i][j] = q;
+    long double distance;
+    long double velocity;
+    struct lip *next;
+};
+void scan(struct lip *lip, int max)
+{
+    for (int i = 0; i < max; i++)
+    {
+        scanf("%Le %Le", &lip->distance, &lip->velocity);
+        if (i < max - 1)
+        {
+            lip->next = (struct lip *)malloc(sizeof(struct lip));
+            lip = lip->next;
         }
     }
-
-//  ====================================================================================
-//                              ПРОВЕРКА НА ПРОСТОЙ ОТВЕТ
-//  ====================================================================================
-
-    double i = -1000;
-
-    i = (double) bubbleSort(mainArr, nParts) * -1;
-    //float min = i;
-    double value = 0.0;
-
-    for (i = i; i < 1001; ++i) {
-        if (testValue(i, mainArr, t, nParts, &value, count)) {
-            printf("%.5f\n", i);
-            return 0;
-        } if (value == -1) break;
+}
+void freee(struct lip *l, long double size, int i)
+{
+    if (i < size - 1)
+    { 
+        freee(l->next,size,i+=1);
     }
-    //printf("save: %f\n", save);
-    //printf("last i: %f\n", i);
-
-    long double from = i-2;
-    long double to = i;
-
-    //printf("from: %f\n", from);
-    //printf("to : %f\n", to);
-    //printf("=====================\n");
-
-//  ====================================================================================
-//                              'К' С ПЛАВАЮЩЕЙ ТОЧКОЙ
-//  ====================================================================================
-    value = 0.0;
-    count = 1;
-    bool same = false;
-
-    for (long double i = from; i < to; i += 0.000001) {
-        if (testValue(i, mainArr, t, nParts, &value, count)) {
-            printf("%.5Lf\n", i);
-            return 0;
-        } if (value == -1) {
-            to = i-0.000001;
-            same = true;
+    free(l);
+}
+long double result(struct lip *lip, long double hoal, long double left, long double right, int lim, int iteration, long double size)
+{
+    long double sample = (left + right) / 2;
+    long double cas = 0;
+    struct lip *l = lip;
+    int i = 0;
+    while (1)
+    {
+        if ((l->velocity + sample) <= 0.0)
+            return result(lip, hoal, sample, right, lim, iteration += 1,size);
+        cas += (l->distance / (l->velocity + sample));
+        if (i >= size -1)
             break;
-        }
-        count = 2;
+        l = l->next;
+        i++;
     }
-    //printf("sisisisisivis\n");
-
-    if (same) printf("%.5Lf\n", to);
-
-    return 0;
+    if (iteration >= lim)
+        return sample;
+    if (cas < hoal)
+        return result(lip, hoal, left, sample, lim, iteration += 1,size);
+    else
+        return result(lip, hoal, sample, right, lim, iteration += 1,size);
 }
-
-
-int main(void)
+int main()
 {
-    rac();
+
+    long double s, res = 0;
+    scanf("%Le %Le", &s, &res);
+    struct lip *lip = (struct lip *)malloc(sizeof(struct lip));
+    // lip->distance = 0;
+    // lip->velocity = 0;
+    // lip->next = NULL;
+    scan(lip, s);
+    long double vl = result(lip, res, -10000, 10000, 200, 0,s);
+    freee(lip,s,0);
+    printf("%.6Lf", vl);
     return 0;
 }
-
-
-int bubbleSort (int arr[][2], int n) {
-    int i, j;
-
-    int qwerty[n];
-    for (int i = 0; i < n; ++i) qwerty[i] = arr[i][1];
-
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (qwerty[j] > qwerty[j+1]) {
-                int tmp = qwerty[j];
-                qwerty[j] = qwerty[j+1];
-                qwerty[j+1] = tmp;
-            }
-        }
-    }
-
-    return qwerty[0];
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//979.000490763
-
-// 998.00085m3868
