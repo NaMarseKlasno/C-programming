112fce06fa062c37bb13dcdd5e4cc77b422d40ae
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-03-24 22:31:25 +0000

    Update racer.c

--- ps2/racer.c
+++ ps2/racer.c
@@ -8,24 +8,42 @@
 
 int bubbleSort (int arr[][2], int n);
 
-bool testValue(long double k, int arr[][2], int t, int parts, double *value, short count) {
-    double res = 0;
+bool testValue2(long double k, int arr[][2], int t, int parts, long double *value) {
+    long double res = 0;
     for (int i = 0; i < parts; ++i) {
         if ( ((long double)arr[i][1] + k) != 0 ) {
             res += (long double)arr[i][0] / ((long double)arr[i][1] + k);
-        }
+        } else return false;
+    } if (*value == -103725598) return false;
+    //printf("2k: %.9Lf -> 2time: %.9Lf\n", k, res);
+    *value = res;
+    if (*value >= (double)t) {
+        return true;
     }
-    //printf("k: %Lf -> time: %Lf\n", k, res);
+    else {
+        *value = res;
+        return false;
+    }
+}
+
+bool testValue(long double k, int arr[][2], int t, int parts, long double *value, short count) {
+    long double res = 0;
+    for (int i = 0; i < parts; ++i) {
+        if ( ((long double)arr[i][1] + k) != 0 ) {
+            res += (long double)arr[i][0] / ((long double)arr[i][1] + k);
+        } else return false;
+    }
+    //printf("k: %.9Lf -> time: %.9Lf\n", k, res);
     
     if (count == 1) {
-        if (*value > res) *value = -1;
+        if (*value >= res) *value = -10372;
         else *value = res;
     }
     
     if (count == 2) {
         static short nw = 0;
         if (*value <= (long double)t && nw != 0) {
-            *value = -1;
+            *value = -10372;
         }
         else *value = res;
         nw = 1;
@@ -49,27 +67,33 @@ int rac (void)
             scanf("%i", &q);
             mainArr[i][j] = q;
         }
-    }
+    } 
     
 //  ====================================================================================
 //                              ПРОВЕРКА НА ПРОСТОЙ ОТВЕТ
 //  ====================================================================================
     
-    long double i = -10000;
-    double value = 0.0;
-    long double from = i-2;
-    long double to = i;
-
-    i = (long double) bubbleSort(mainArr, nParts) * -1;
+    long double i = -1001;
+    bool stop1 = true;
     
-    for (i = i; i <= 1001; ++i) {
+    i = (double) bubbleSort(mainArr, nParts) * -1;
+    long double value = 0.0;
+    
+    for (i = i; i <= 1002; ++i) {
         if (testValue(i, mainArr, t, nParts, &value, count)) {
             printf("%.6Lf\n", i);
             return 0;
-        } if (value == -1) break;
+        } if (value == -10372) break;
+        
+        
+        stop1 = false;
+
     }
     //printf("last i: %Lf\n", i);
     
+    long double from = i-2;
+    long double to = i;
+
     //printf("from: %Lf\n", from);
     //printf("to : %Lf\n", to);
     //printf("=====================\n");
@@ -79,18 +103,31 @@ int rac (void)
 //  ====================================================================================
     value = 0.0;
     count = 1;
-    bool same = false;
+    bool same = false, stop = true;
     
-    for (long double i = from; i <= 10000.0; i += 0.000000001) {
+    for (long double i = from; i < to; i += 0.000000001) {
         if (testValue(i, mainArr, t, nParts, &value, count)) {
             printf("%.6Lf\n", i);
             return 0;
-        } if (value == -1) {
+        }
+        if (value == -10372) {
             to = i-0.000000001;
             same = true;
             break;
-        }
-        count = 2;
+        } count = 2;
+        
+        while (stop == true && testValue2(i+1, mainArr, t, nParts, &value)) i+=1;
+        while (stop == true && testValue2(i+0.1, mainArr, t, nParts, &value)) i+=0.1;
+        while (stop == true && testValue2(i+0.01, mainArr, t, nParts, &value)) i+=0.01;
+        while (stop == true && testValue2(i+0.001, mainArr, t, nParts, &value)) i+=0.001;
+        while (stop == true && testValue2(i+0.0001, mainArr, t, nParts, &value)) i+=0.0001;
+        while (stop == true && testValue2(i+0.00001, mainArr, t, nParts, &value)) i+=0.00001;
+        while (stop == true && testValue2(i+0.000001, mainArr, t, nParts, &value)) i+=0.000001;
+        while (stop == true && testValue2(i+0.0000001, mainArr, t, nParts, &value)) i+=0.0000001;
+        while (stop == true && testValue2(i+0.00000001, mainArr, t, nParts, &value)) i+=0.00000001;
+        while (stop == true && testValue2(i+0.000000001, mainArr, t, nParts, &value)) i+=0.000000001;
+
+        stop = false;
     }
     
     
