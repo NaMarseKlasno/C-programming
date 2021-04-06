507f9b7248c1a91e68c060b43233b2b9819e3163
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-04-06 21:32:41 +0000

    Update ui.h

--- ps3/ui.h
+++ ps3/ui.h
@@ -1,2 +1,13 @@
-#include <stdio.h>
+#ifndef K_UI_H
+#define K_UI_H
+
+#include "k.h"
+
+/**
+ * Renders the game
+ * @param game the game object with board and score to render
+ */
+void render(const struct game game);
+
+#endif 
 
