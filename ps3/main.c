103948785b97381b8426d34c741c8e67e5c4be96
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-04-06 20:39:48 +0000

    Update main.c

--- ps3/main.c
+++ ps3/main.c
@@ -1,40 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "k.h"
+#include "ui.h"
 #include "hof.h"
 
-int main() {
-    // move right
+int main()
+{
+    struct player *list = malloc(sizeof(struct player) * 10);
+    int hof_size = load(list);
+
+    struct player *player1 = malloc(sizeof(player1));
+    player1->score = 12;
+    char *name = "Hello";
+    strcpy(player1->name, name);
+
+    add_player(list, &hof_size, *player1);
+    save(list, hof_size);
+
+    for (int i=0; i<hof_size; i++)
+    {
+        printf("%s\t%d\n", list[i].name, list[i].score);
+    }
+
     struct game game = {
             .board = {
                     {'A', ' ', ' ', ' '},
-                    {'B', ' ', ' ', 'B'},
-                    {'C', 'C', 'C', ' '},
-                    {'D', 'D', 'D', 'D'}
+                    {' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' '},
+                    {'A', ' ', ' ', ' '}
             },
             .score = 0
     };
 
-    bool result = update(&game, 0, 1);
-/*
-game = {
-    .board = {
-        {' ', ' ', ' ', 'A'},
-        {' ', ' ', ' ', 'C'},
-        {' ', ' ', 'C', 'D'},
-        {' ', ' ', 'E', 'E'}
-    },
-    .score = 88
-};
-result = true;
-*/
+    render(game);
 
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; ++j) {
-            printf("%c  ", game.board[i][j]);
-        } printf("\n");
+    while (true)
+    {
+        int flag = getchar();
+        switch (flag)
+        {
+            case 119:
+                if (update(&game, -1, 0)) add_random_tile(&game);
+                render(game);
+                break;
+            case 97:
+                if (update(&game, 0, 1)) add_random_tile(&game);
+                render(game);
+                break;
+            case 115:
+                if (update(&game, 1, 0)) add_random_tile(&game);
+                render(game);
+                break;
+            case 100:
+                if (update(&game, 0, -1)) add_random_tile(&game);
+                render(game);
+                break;
+            case 101:
+                free(list);
+                free(player1);
+                return 0;
+            default:
+                break;
+        }
     }
-
-    
-
-    return 0;
 }
