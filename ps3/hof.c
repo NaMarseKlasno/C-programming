ba7d82cfd0c0e86ddb5d9d16605d5d1853e68a63
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-04-07 21:50:55 +0000

    Update hof.c

--- ps3/hof.c
+++ ps3/hof.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+/*
 int str_to_num(char *str)
 {
     int result = 0, same = 1;
@@ -16,45 +19,86 @@ int str_to_num(char *str)
     }
     return (result * same);
 }
+*/
+void bubbleSort (struct player qwerty[], int n) {
+    int i, j;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (qwerty[j].score < qwerty[j+1].score) {
+                int tmp = qwerty[j].score;
+
+                qwerty[j].score = qwerty[j+1].score;
+                qwerty[j+1].score = tmp;
+
+                char tmp2[31] = {};
+                for (int k = 0; k < strlen(qwerty[j].name)+1; ++k) tmp2[k] = qwerty[j].name[k];
+
+                strcpy(qwerty[j].name, qwerty[j+1].name);
+                for (int k = 0; k < strlen(tmp2)+1; ++k) qwerty[j+1].name[k] = tmp2[k];
+            }
+        }
+
+    }
+}
+
 
 int load (struct player list[])
 {
     FILE *file;
-    if ((file = fopen("score", "r")) == NULL) {
-        printf("Ошибка чтения из файла\n");
-        return 0;
-    }
+    if ((file = fopen("score", "r")) == NULL) return -1;
+
 
     char a[100000] = {};
-    char str[100000] = {};
-    int value[100000] = {};
+    char value[3000] = {};
+    int n = 0;
+    bool check = true;
+
+    struct player *lines = calloc(100000, sizeof(struct player));
+
+    for (int i = 0; (fgets(a, 100000, file) != NULL); ++i) {
+        char str[30] = {};
+
+        for (int j = 0, q = 0; a[j] != '\0'; j++) {
+            if (a[j] != ' ' && check == true) str[j] = a[j];
+            else {
+                check = false;
+                if (a[j] != ' ') {
+                    value[q] = a[j];
+                    q++;
+                }
+            }
+        } check = true;
+
+
+        for (int j = 0; str[j] != '\0'; ++j) lines[i].name[j] = str[j];
+        int num = 0;
+        if (isdigit(value[0])) num = atoi(value);
+        //else printf("INPUT ERROR\n");
+        lines[i].score = num;
+        n++;
+    }
+    //for (int i = 0; i < n; ++i) printf("%s %d\n", lines[i].name, lines[i].score);
+
+    bubbleSort(lines, n);
 
     for (int i = 0; i < 10; ++i) {
-        fgets(a, 10000, file);
-
-        for (int i = 0, q = 0; a[i] != '\0'; i++) {
-            if (!isnumber(a[i])) str[i] = a[i];
-            else if (a[i] != ' ') {
-                value[q] = str_to_num(&a[i]);
-                q++;
-            }
-        }
-
-
-
-        for (int j = 0; str[j] != '\0'; ++j)
-            list[0].name[j] = str[j];
-
-
-        int num = 0;
-        isdigit(value[0]) ? (num = atoi((const char *) value)) : printf("INPUT ERROR\n");
-
-        list[0].score = num;
-
+        //list[i].name = lines[i].name;
+        strcpy(list[i].name, lines[i].name);
+        list[i].score = lines[i].score;
     }
 
+    free(lines);
     fclose(file);
+    return 0;
+}
 
+bool save(const struct player list[], const int size) {
+    return 0;
+}
+
+
+bool add_player(struct player list[], int* size, const struct player player) {
     return 0;
 }
 
