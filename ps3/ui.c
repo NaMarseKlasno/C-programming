3f97aa90cf37c79951cef9a10ec81d3cfc9345d8
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-04-07 17:45:49 +0000

    Update ui.c

--- ps3/ui.c
+++ ps3/ui.c
@@ -4,19 +4,19 @@
 
 #include "k.h"
 
-//void render(const struct game game){
-//    printf("score: %d\n", game.score);
-//
-//    for(int row = 0; row < SIZE; row++){
-//        printf("+---+---+---+---+\n|");
-//        for(int col = 0; col < SIZE; col++){
-//            printf(" %c |", game.board[row][col]);
-//        }
-//        printf("\n");
-//    }
-//    printf("+---+---+---+---+\n");
-//}
+void render(const struct game game){
+    printf("score: %d\n", game.score);
 
+    for(int row = 0; row < SIZE; row++){
+        printf("+---+---+---+---+\n|");
+        for(int col = 0; col < SIZE; col++){
+            printf(" %c |", game.board[row][col]);
+        }
+        printf("\n");
+    }
+    printf("+---+---+---+---+\n");
+}
+/*
 void render(const struct game game){
     clear();
     char text[20];
@@ -56,3 +56,4 @@ void render(const struct game game){
 
     refresh();
 }
+*/
