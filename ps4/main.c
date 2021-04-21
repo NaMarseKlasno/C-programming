cb7e1728f7bbd6cbc4866720fc7ca798b2c644e9
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-04-22 01:11:01 +0300

    update main.c (for test)

--- ps4/main.c
+++ ps4/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "bmp.h"
 
-
-int main (void)
+int main()
 {
-    FILE* stream = "Qk1WAAAAAAAAADYAAAAoAAAAAgAAAAQAAAABABgAAAAAACAAAAAjLgAAIy4AAAAAAAAAAAAA////AAAAAAD/AAAA/wAAAAD/AAAA/wAAAAAA/wAAAAA="; // base64 encoded stream
+    FILE * file = fopen("assets/cj.bmp", "rb");
+    if (file == NULL) {
+        printf("ERROR\n");
+        return 1;
+    }
+    struct bmp_header *header = calloc(1, sizeof(struct bmp_header));
+    header = read_bmp_header(file);
 
+    struct pixel* pixels = calloc(header->height*header->width, sizeof(struct pixel));
+    pixels = read_data(file, header);
+
+    for (int i = 0; i < header->width*header->height ; ++i) {
+        printf("R:%d G:%d B:%d\n", pixels[i].red, pixels[i].green, pixels[i].blue);
+    }
+
+
+    fclose(file);
+    free(pixels);
     return 0;
 }
-
-//echo "Qk1WAAAAAAAAADYAAAAoAAAAAgAAAAQAAAABABgAAAAAACAAAAAjLgAAIy4AAAAAAAAAAAAA////AAAAAAD/AAAA/wAAAAD/AAAA/wAAAAAA/wAAAAA="
