9d53cce1aab47407746493c668e7856d390bb40b
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-05-10 17:43:10 +0300

    neuzheli

--- ps5/container.c
+++ ps5/container.c
@@ -6,7 +6,6 @@
 
 void destroy_next_conts (struct container* next);
 int check_string2 (char *str_one, char *str_two);
-int check_symbol2 (char c);
 
 
 struct container* create_container (struct container* first, enum container_type type, void* entry) {
@@ -121,29 +120,10 @@ void destroy_next_conts (struct container* next) {
     //next = NULL;
 }
 
-int check_symbol2 (char c) { // pomocka -> https://www.cyberforum.ru/c-beginners/thread2191515.html
-    int answer = -1;
-    if (c >= 'a' && c <= 'z') answer = 0;
-    else if (c >= 'A' && c <= 'Z') answer = 1;
-    return answer;
-}
-
 int check_string2 (char *str_one, char *str_two) {
     if (str_two == NULL || str_one == NULL) return 1;
     if (strlen(str_one) != strlen(str_two)) return 1;
-    char tmp; int eq = 1;
-
-    for (int i=0, c1, c2, swp = 'a' - 'A'; str_one[i] != '\0' && eq > 0; i++)
-    {
-        tmp = str_two[i];
-        c1 = check_symbol2(str_one[i]);
-        c2 = check_symbol2(str_two[i]);
-        if (c1 < c2) tmp += (char)swp;
-        if (c1 > c2) tmp -= (char)swp;
-        if (str_one[i] != tmp) eq = 0;
-    }
-
-    if (eq > 0) return 0;
-    return 1;
+    for (int i = 0; i < strlen(str_one); ++i) if (toupper(str_one[i]) != toupper(str_two[i])) return 1;
+    return 0;
 }
 
