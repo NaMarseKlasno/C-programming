88b0429fbd7a41528d35ad7de41a636f73bfde77
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-05-15 22:08:58 +0300

    regex fixes

--- ps5/parser.c
+++ ps5/parser.c
@@ -32,27 +32,27 @@ struct parser* create_parser () {
     if (pars == NULL) return NULL;
 
     // ***** add basic commands to pars
-    pars->commands = create_container(NULL, COMMAND, create_command("KONIEC", "Príkaz ukončí rozohratú hru. Nastaví príslušný stav hry.", "(^\\s{0,}(QUIT|EXIT)\\s{0,}$)", 2));
+    pars->commands = create_container(NULL, COMMAND, create_command("KONIEC", "Príkaz ukončí rozohratú hru. Nastaví príslušný stav hry.", "(^\\s{0,}(KONIEC|QUIT|EXIT)\\s{0,}$)", 3));
 
-    create_container(pars->commands, COMMAND, create_command("SEVER", "Presun do miestnosti nachádzajúcej sa na sever od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(S)\\s{0,}$)", 1));
-    create_container(pars->commands, COMMAND, create_command("JUH", "Presun do miestnosti nachádzajúcej sa na juh od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(J)\\s{0,}$)", 1));
-    create_container(pars->commands, COMMAND, create_command("VYCHOD", "Presun do miestnosti nachádzajúcej sa na východ od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(V)\\s{0,}$)", 1));
-    create_container(pars->commands, COMMAND, create_command("ZAPAD", "Presun do miestnosti nachádzajúcej sa na západ od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(Z)\\s{0,}$))", 1));
-    create_container(pars->commands, COMMAND, create_command("ROZHLIADNI SA", "Príkaz vypíše aktuálne informácie o miestnosti, v ktorej sa hráč práve nachádza.", "", 0));
-    create_container(pars->commands, COMMAND, create_command("PRIKAZY", "Príkaz vypíše na obrazovku zoznam všetkých príkazov, ktoré hra poskytuje.", "(^\\s{0,}(HELP|POMOC)\\s{0,}$)", 2));
-    create_container(pars->commands, COMMAND, create_command("VERZIA", "Príkaz zobrazí číslo verzie hry, ľubovoľný sprievodný text a meno a priezvisko autora s kontaktom (e-mailová adresa, webová stránka).", "", 0));
-    create_container(pars->commands, COMMAND, create_command("RESTART", "Znovu spustí hru od začiatku. Zmení stav hry na požadovaný.", "", 0));
-    create_container(pars->commands, COMMAND, create_command("O HRE", "Príkaz zobrazí krátky text, ktorý poslúži ako úvod do príbehu. Ako dobrý začiatok sa javí známy text: Kde bolo tam bolo, …", "(^\\s{0,}(ABOUT)\\s{0,}$)", 1) );
-    create_container(pars->commands, COMMAND, create_command("VEZMI", "Vloží predmet z miestnosti do batohu. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš vziať.).", "", 0));
-    create_container(pars->commands, COMMAND, create_command("POLOZ", "Položí predmet z batohu do miestnosti. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš položiť.)", "", 0));
-    create_container(pars->commands, COMMAND, create_command("INVENTAR", "Zobrazí obsah hráčovho batohu.", "(^\\s{0,}(I)\\s{0,}$)", 1));
-    create_container(pars->commands, COMMAND, create_command("POUZI", "Použije predmet z batohu alebo miestnosti. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš použiť.).", "", 0));
-    create_container(pars->commands, COMMAND, create_command("PRESKUMAJ", "Vypíše opis predmetu, ktorý sa musí nachádzať v miestnosti alebo batohu. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný alebo sa nenájde v batohu alebo v miestnosti, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš preskúmať.).", "", 0));
-    create_container(pars->commands, COMMAND, create_command("NAHRAJ", "Príkaz zabezpečí nahratie uloženej pozície hry z disku. Voliteľným parametrom je cesta k súboru.", "(^\\s{0,}(LOAD)\\s{0,}$)", 1));
-    create_container(pars->commands, COMMAND, create_command("ULOZ", "Príkaz uloží stav rozohratej hry na disk. Voliteľným parametrom je cesta k súboru.", "(^\\s{0,}(SAVE)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("SEVER", "Presun do miestnosti nachádzajúcej sa na sever od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(SEVER|S)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("JUH", "Presun do miestnosti nachádzajúcej sa na juh od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(JUH|J)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("VYCHOD", "Presun do miestnosti nachádzajúcej sa na východ od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(VYCHOD|V)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("ZAPAD", "Presun do miestnosti nachádzajúcej sa na západ od aktuálnej. Zmení referenciu aktuálnej miestnosti.", "(^\\s{0,}(ZAPAD|Z)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("ROZHLIADNI SA", "Príkaz vypíše aktuálne informácie o miestnosti, v ktorej sa hráč práve nachádza.", "(^\\s{0,}ROZHLIADNI\\sSA\\s{0,}$))", 1));
+    create_container(pars->commands, COMMAND, create_command("PRIKAZY", "Príkaz vypíše na obrazovku zoznam všetkých príkazov, ktoré hra poskytuje.", "(^\\s{0,}(PRIKAZY|HELP|POMOC)\\s{0,}$)", 3));
+    create_container(pars->commands, COMMAND, create_command("VERZIA", "Príkaz zobrazí číslo verzie hry, ľubovoľný sprievodný text a meno a priezvisko autora s kontaktom (e-mailová adresa, webová stránka).", "(^\\s{0,}(VERZIA)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("RESTART", "Znovu spustí hru od začiatku. Zmení stav hry na požadovaný.", "(^\\s{0,}(RESTART)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("O HRE", "Príkaz zobrazí krátky text, ktorý poslúži ako úvod do príbehu. Ako dobrý začiatok sa javí známy text: Kde bolo tam bolo, …", "(^\\s{0,}(O HRE|ABOUT)\\s{0,}$)", 2) );
+    create_container(pars->commands, COMMAND, create_command("VEZMI", "Vloží predmet z miestnosti do batohu. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš vziať.).", "(^\\s{0,}(VEZMI)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("POLOZ", "Položí predmet z batohu do miestnosti. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš položiť.)", "(^\\s{0,}(POLOZ)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("INVENTAR", "Zobrazí obsah hráčovho batohu.", "(^\\s{0,}(INVENTAR|I)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("POUZI", "Použije predmet z batohu alebo miestnosti. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš použiť.).", "(^\\s{0,}(POUZI)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("PRESKUMAJ", "Vypíše opis predmetu, ktorý sa musí nachádzať v miestnosti alebo batohu. Príkaz má jeden povinný parameter, ktorým je názov predmetu. Ak predmet nebude zadaný alebo sa nenájde v batohu alebo v miestnosti, program vypíše na obrazovku vhodnú hlášku (napr. Neviem, čo chceš preskúmať.).", "(^\\s{0,}(PRESKUMAJ)\\s{0,}$)", 1));
+    create_container(pars->commands, COMMAND, create_command("NAHRAJ", "Príkaz zabezpečí nahratie uloženej pozície hry z disku. Voliteľným parametrom je cesta k súboru.", "(^\\s{0,}(NAHRAJ|LOAD)\\s{0,}$)", 2));
+    create_container(pars->commands, COMMAND, create_command("ULOZ", "Príkaz uloží stav rozohratej hry na disk. Voliteľným parametrom je cesta k súboru.", "(^\\s{0,}(ULOZ|SAVE)\\s{0,}$)", 2));
 
     // ***** add history to res_pars && release memory
-    pars->history = create_container(NULL, COMMAND,  create_command("O HRE", "Príkaz zobrazí krátky text, ktorý poslúži ako úvod do príbehu. Ako dobrý začiatok sa javí známy text: Kde bolo tam bolo, …", "(^\\s{0,}(ABOUT)\\s{0,}$)", 1));
+    pars->history = create_container(NULL, COMMAND,  create_command("O HRE", "Príkaz zobrazí krátky text, ktorý poslúži ako úvod do príbehu. Ako dobrý začiatok sa javí známy text: Kde bolo tam bolo, …", "(^\\s{0,}(O HRE|ABOUT)\\s{0,}$)", 2));
     if (pars->history == NULL) {
         destroy_parser(pars);
         return NULL;
